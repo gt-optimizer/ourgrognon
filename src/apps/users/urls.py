f65ea52d87b14256c5722d95b141fde55d8cc638
optimizerlabs <optimizerlabs@optimizer-labs.fr>
2026-03-14 18:10:45 +0100

    rajout modification changement mdp

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -12,4 +12,15 @@ urlpatterns = [
     ), name='login'),
     path('deconnexion/', auth_views.LogoutView.as_view(), name='logout'),
     path('profil/', views.profile, name='profile'),
+    path('mot-de-passe/modifier/',
+         auth_views.PasswordChangeView.as_view(
+             template_name='users/password_change.html',
+             success_url='/mot-de-passe/modifier/succes/'
+         ),
+         name='password_change'),
+    path('mot-de-passe/modifier/succes/',
+         auth_views.PasswordChangeDoneView.as_view(
+             template_name='users/password_change_done.html'
+         ),
+         name='password_change_done'),
 ]
